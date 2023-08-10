92882513476460bd8f2175f1b2d6d2fed9a0bf2b
Ando <iamando@tuta.io>
2023-08-10 20:27:22 +0300

    feature: create stream utils and setup tcp listener to bind addr from client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{net::TcpListener, thread};
+
+use crate::utils::stream::handle_client;
+
+mod utils {
+  pub mod stream;
+}
+
 fn main() {
-    println!("Hello, world!");
+  let addr = "127.0.0.1:4000";
+  let listener = TcpListener::bind(addr).unwrap();
+
+  for stream in listener.incoming() {
+    match stream {
+      Ok(stream) => {
+        thread::spawn(move || handle_client(stream));
+      }
+      Err(err) => {
+        panic!("{}", err)
+      }
+    }
+  }
 }
---
+++ src/utils/stream.rs
@@ -0,0 +1,22 @@
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpStream;
+
+pub fn handle_client(mut stream: TcpStream) {
+  loop {
+    let mut read = [0; 1028];
+
+    match stream.read(&mut read) {
+      Ok(n) => {
+        if n == 0 {
+          break;
+        }
+
+        stream.write(&read[0..n]).unwrap();
+      }
+      Err(err) => {
+        panic!("{}", err)
+      }
+    }
+  }
+}
