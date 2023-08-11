19ed44930cfe6409e647456a3d53fedad97e39ef
Ando <iamando@tuta.io>
2023-08-11 20:37:06 +0300

    feature: impl enum http status code

--- src/http.rs
+++ src/http.rs
@@ -14,3 +14,20 @@ pub enum HttpMethod {
   TRACE,
   OTHER(String),
 }
+
+#[derive(Debug, Default)]
+pub enum HttpStatusCode {
+  #[default]
+  Success = 200,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  NotAcceptable = 406,
+  Conflict = 409,
+  InternalServerError = 500,
+  NotImplemented = 501,
+  BadGateway = 502,
+  ServiceUnavailable = 503,
+}
